bbcfa5f9d086bc8128a5d09bb54a6885ac40a23b
taiga0213 <kuronosx@yahoo.co.jp>
2015-05-19 10:48:27 +0900

    テストその2

--- src/jp/ac/hal/skymoons/test/TestServlet.java
+++ src/jp/ac/hal/skymoons/test/TestServlet.java
@@ -10,10 +10,10 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class TestServlet
  */
-@WebServlet("/TestServlet")
+@WebServlet("/")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
+
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,6 +27,7 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("Hello World! [SkyMoons]");
 	}
 
 	/**
