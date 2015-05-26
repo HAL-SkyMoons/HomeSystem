4a40161eed83f4ec532911a2f6054714e60222d2
taiga0213 <kuronosx@yahoo.co.jp>
2015-05-26 11:11:00 +0900

    FrontController改革

--- src/jp/ac/hal/skymoons/controllers/FrontController.java
+++ src/jp/ac/hal/skymoons/controllers/FrontController.java
@@ -1,14 +1,6 @@
 package jp.ac.hal.skymoons.controllers;
 
-import jp.ac.hal.skymoons.models.ModelSelector;
-import jp.ac.hal.skymoons.selectors.DataSelector;
-import jp.ac.hal.skymoons.selectors.ForwardPageSelector;
-import jp.ac.hal.skymoons.selectors.RequestNameSelector;
-
 import java.io.IOException;
-import java.sql.SQLException;
-
-import javax.naming.NamingException;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -16,102 +8,81 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import jp.ac.hal.skymoons.core.Core;
-import jp.ac.hal.skymoons.dao.MysqlDao;
-import jp.ac.hal.skymoons.data.MysqlData;
-import jp.ac.hal.skymoons.selectors.RequestColumsSelector;
-
 /**
  * Servlet implementation class FrontController
  */
-@WebServlet("/admin/exec/*")
+@WebServlet("/fcon/*")
 public class FrontController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public FrontController() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		//文字コード設定等
-		response.setContentType("text/html");
-		response.setCharacterEncoding("UTF-8");
-		request.setCharacterEncoding("UTF-8");
-		
-		//URI取得
-		String uri = request.getRequestURI();
-		
-		//モデルの選択
-		Core core = ModelSelector.choice(uri);
-		
-		//使用するデータの選択
-		DataSelector ds = new DataSelector();
-		MysqlData mData = ds.choice(uri);
+	public FrontController() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
 
-		//リクエスト値をdataにセット
-		RequestColumsSelector rgs = new RequestColumsSelector();
-		rgs.setValues(request, mData);
-		
-		//coreにMysqlDataのセット
-		core.setMysqlData(mData);
-		
-		//データベース処理
-		try{
-			//DAOの生成とセット
-			MysqlDao mysqlDao = new MysqlDao();
-			core.setMysqlDao(mysqlDao);
-			try{
-				//SQLの実行
-				//SELECTであればmDataへ値がセットされる
-				core.executeSQL();
-				//処理確定
-				core.getMysqlDao().commit();
-			}catch(Exception e){
-				//処理取り消し
-				core.getMysqlDao().rollback();
-				e.printStackTrace();
-			}finally{
-				core.getMysqlDao().close();
-			}
-		}catch(SQLException e){
-			//エラー処理 SQL実行エラー?
-			e.printStackTrace();
-		}catch(NamingException e){
-			//エラー処理 初期コンテキスト等
-			e.printStackTrace();
-		}catch(Exception e){
-			//その他のエラー処理
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("fconGET!");
+
+		// リクエストのあったURIを取得
+		String requestURI = request.getRequestURI();
+
+		// 対象となるモデルを取得
+		AbstractModel targetModel = ModelSelectorGet.select(requestURI);
+		System.out.println("targetModel:" + targetModel);
+
+		// 業務ロジックを実行しJSP名を取得
+		String jspName = "/jsp/error.jsp";
+		try {
+			jspName = targetModel.doService(request, response);
+			System.out.println(jspName);
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		//リクエストに値を設定
-		if(core.getList() != null && RequestNameSelector.getRequestName(uri) != null){
-			request.setAttribute(RequestNameSelector.getRequestName(uri), core.getList());
-		}
-		
-		//次ページ移動処理
-		RequestDispatcher dispatcher;
-		
-		//移動先の取得
-		String forward = ForwardPageSelector.choice(uri);
-		dispatcher = request.getRequestDispatcher(forward);
-		
-		//移動処理
+
+		// fowerd
+		RequestDispatcher dispatcher = request.getRequestDispatcher(jspName);
 		dispatcher.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		System.out.println("fconPOST!");
 
+		// リクエストのあったURIを取得
+		String requestURI = request.getRequestURI();
+
+		// 対象となるモデルを取得
+		AbstractModel targetModel = ModelSelector.select(requestURI);
+		System.out.println("targetModel:" + targetModel);
+
+		// Daoを設定
+		// targetModel.setLanguage(languageDao);
+
+		// 業務ロジックを実行しJSP名を取得
+		String jspName = "/jsp/error.jsp";
+		try {
+			jspName = targetModel.doService(request, response);
+		} catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		System.out.println(jspName);
+
+		// fowerd
+		RequestDispatcher dispatcher = request.getRequestDispatcher(jspName);
+		dispatcher.forward(request, response);
+	}
 }
