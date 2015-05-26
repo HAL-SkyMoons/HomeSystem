8144dae30454676d77b8782d82033eca91987b40
IT11-山崎現 <IT11-@172.29.1.51>
2015-05-26 12:40:22 +0900

    q

--- src/jp/ac/hal/skymoons/models/TestModel.java
+++ src/jp/ac/hal/skymoons/models/TestModel.java
@@ -12,12 +12,6 @@ public class TestModel extends AbstractModel{
 			HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
 
-<<<<<<< HEAD
-		//
-=======
-		//test
->>>>>>> refs/remotes/origin/YAMAZAKI-GEN
-
 		return "/error.html"	;
 	}
 
