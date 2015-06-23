7afe0f553baf8c73e4f7c1975fae62565b19b21a
splay20501 <splay20501@hotmail.co.jp>
2015-06-23 12:34:31 +0900

    ContentsDetail v0.14 添付資料のダウンロード関連

--- WebContent/contents/detail.jsp
+++ WebContent/contents/detail.jsp
@@ -1,4 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<%@ page import="jp.ac.hal.skymoons.util.Utility"%>
+<%@ page import="jp.ac.hal.skymoons.beans.ContentsDataBean"%>
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <!DOCTYPE html>
 <html>
@@ -29,20 +31,35 @@
 						<c:out value="${genreName}"/>
 					</c:forEach>
 				</td>
-				<td>コンテンツ内容：${i.homeContentComment}</td>
-				<td>添付資料：
-					<c:set var="homeDataIndex" value="0"/>
-					<c:forEach items="${i.homeDataNo}" var="homeDataNo">
-						<c:forEach items="${i.homeDataName}" begin="${homeDataIndex}" end="${homeDataIndex}" var="homeDataName">
-							<a href='../../files/master/${homeDataNo}'>${homeDataName}</a>
-						</c:forEach>
-						<c:set var="homeDataIndex" value="${homeDataIndex + 1}"/>
-					</c:forEach>
-				</td>
 			</tr>
 		</table>
-		<table border="1">
-			<c:forEach items="${homeLogList}" var="homeLog">
+		
+		<table>
+			<tr>
+				<th>No</th>
+				<th>画像</th>
+				<th>ファイル名</th>
+				<th>ダウンロード</th>
+			</tr>
+			<c:forEach items="${dataList}" var="j">
+				<tr>
+					<td>${j.homeDataNo}</td>
+					<td><img src="${j.fileImagePath}" width="50" height="50"></td>
+					<td>${j.homeDataName}</td>
+					<td>
+						<form action="/HomeSystem/fc/contents/detail?homeContentId=${i.homeContentId}" method="post">
+							<input type="hidden" name="homeContentId" value="${j.homeContentId}">
+							<input type="hidden" name="path" value="../files/contents/master/${j.homeContentId}/${j.homeDataNo}/${j.homeDataName}" />
+							<input type="hidden" name="fileName" value="${j.homeDataName}"/>
+							<input type="submit" name="download" value="ダウンロード">
+						</form>
+					</td>
+				</tr>
+			</c:forEach>
+		</table>
+		
+		<c:forEach items="${homeLogList}" var="homeLog">
+			<table border="1">
 				<tr>
 					<td>ほめられユーザー：
 						<a href="">${homeLog.homeTargetFirstName}${homeLog.homeTargetLastName}</a>
@@ -55,7 +72,7 @@
 					<td>付与ポイント：${homeLog.homePoint}</td>
 					<td>ほめコメント${homeLog.homeComment}</td>
 				</tr>
-			</c:forEach>
-		</table>
+			</table>
+		</c:forEach>
 	</body>
 </html>
--- src/jp/ac/hal/skymoons/models/ContentsDetailModel.java
+++ src/jp/ac/hal/skymoons/models/ContentsDetailModel.java
@@ -1,14 +1,28 @@
 package jp.ac.hal.skymoons.models;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+
+import jp.ac.hal.skymoons.beans.CommentBean;
+import jp.ac.hal.skymoons.beans.ContentsDataBean;
 import jp.ac.hal.skymoons.beans.ContentsDetailBean;
 import jp.ac.hal.skymoons.beans.ContentsDetailHomeLogBean;
+import jp.ac.hal.skymoons.beans.FileBean;
 import jp.ac.hal.skymoons.controllers.AbstractModel;
 import jp.ac.hal.skymoons.daoes.ContentsDetailDao;
+import jp.ac.hal.skymoons.daoes.SampleDao;
+import jp.ac.hal.skymoons.security.session.SessionController;
+import jp.ac.hal.skymoons.util.Utility;
 
 public class ContentsDetailModel extends AbstractModel{
 
@@ -16,20 +30,32 @@ public class ContentsDetailModel extends AbstractModel{
 	public String doService(HttpServletRequest request,
 			HttpServletResponse response) throws Exception {
 		// TODO Auto-generated method stub
-
+		//IDの取得
+		int homeContentId = Integer.parseInt(request.getParameter("homeContentId"));
+		
+		//添付資料ダウンロード処理
+		if (request.getParameter("download") != null) {
+			System.out.println("download");
+			Utility utility = new Utility();
+			utility.download(request, response);
+			return null;
+		}
+		
 		//DAOのインスタンス化
 		ContentsDetailDao dao = new ContentsDetailDao();
 		
 		//検索に使用するDAOを選択しIDを指定
-		String homeContentId = request.getParameter("homeContentId");
 		ContentsDetailBean detailData = null;
 		detailData = dao.findDetail(homeContentId);
 		ArrayList<ContentsDetailHomeLogBean> homeLogData = null;
 		homeLogData = dao.findHomeLog(homeContentId);
+		ArrayList<ContentsDataBean> homeData = null;
+		homeData = dao.findData(homeContentId);
 		
 		//結果をリクエストに保存
 		request.setAttribute("detailList",detailData);
 		request.setAttribute("homeLogList",homeLogData);
+		request.setAttribute("dataList",homeData);
 		
 		//コミットと終了処理
 		dao.commit();
